afa08a7bb74e75926d37992f1260778795258be3
TommyYehCool <tommy.yeh1112@gmail.com>
2016-08-03 17:51:49 +0800

    增加更詳細資料

--- src/main/java/com/exfantasy/example/file/FileDemo.java
+++ src/main/java/com/exfantasy/example/file/FileDemo.java
@@ -1,12 +1,22 @@
 package com.exfantasy.example.file;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 public class FileDemo {
+	private static int KB = 1024;
+	private static int MB = KB * 1024;
+	private static int GB = MB * 1024;
+	private static DecimalFormat format = new DecimalFormat("###,###");
+	
 	public static void main(String[] args) {
+		System.out.printf("KB: %d bytes, MB: %d bytes, GB: %d bytes %n", KB, MB, GB);
+		
 		File[] roots = File.listRoots();
 		for (File root : roots) {
-			System.out.printf("%s 總容量 %d，可用容量 %d %n", root.getPath(), root.getTotalSpace(), root.getUsableSpace());
+			long totalSpace = root.getTotalSpace();
+			long usableSpace = root.getUsableSpace();
+			System.out.printf("%s 總容量: %d GB (%s bytes)，可用容量: %d GB (%s bytes)%n", root.getPath(), totalSpace / GB, format.format(totalSpace), usableSpace / GB, format.format(usableSpace));
 		}
 	}
 }
